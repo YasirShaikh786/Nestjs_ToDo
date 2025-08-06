11e00f2579acc3e798c370178edf85a24ba0b042
Yasir__Shaikh <yasirshaikhpune@gmail.com>
2025-08-06 20:44:39 +0530

    backend working and ready.

---
+++ backend/src/app.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { TodosModule } from './todos/todos.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRootAsync({
+      useFactory: () => ({
+        uri: process.env.MONGODB_URI,
+      }),
+    }),
+    TodosModule,
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
---
+++ backend/src/todos/todos.service.ts
@@ -0,0 +1,48 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Todo } from './todo.schema';
+import { CreateTodoDto } from './dto/create-todo.dto';
+import { UpdateTodoDto } from './dto/update-todo.dto';
+
+@Injectable()
+export class TodosService {
+  constructor(@InjectModel(Todo.name) private todoModel: Model<Todo>) {}
+
+  async create(createTodoDto: CreateTodoDto): Promise<Todo> {
+    const createdTodo = new this.todoModel(createTodoDto);
+    return createdTodo.save();
+  }
+
+  async findAll(): Promise<Todo[]> {
+    return this.todoModel.find().exec();
+  }
+
+  async findOne(id: string): Promise<Todo> {
+    const todo = await this.todoModel.findById(id).exec();
+    if (!todo) {
+      throw new NotFoundException(`Todo with ID ${id} not found`);
+    }
+    return todo;
+  }
+
+  async update(id: string, updateTodoDto: UpdateTodoDto): Promise<Todo> {
+    const updatedTodo = await this.todoModel
+      .findByIdAndUpdate(id, updateTodoDto, { new: true })
+      .exec();
+    
+    if (!updatedTodo) {
+      throw new NotFoundException(`Todo with ID ${id} not found`);
+    }
+    
+    return updatedTodo;
+  }
+
+  async remove(id: string): Promise<Todo> {
+    const todo = await this.todoModel.findByIdAndDelete(id).exec();
+    if (!todo) {
+      throw new NotFoundException(`Todo with ID ${id} not found`);
+    }
+    return todo;
+  }
+}
